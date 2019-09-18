379c589771d1855e5e43ae19bdf05b5cfe510d4b
piotrpopiolek <piotr.popiolek93@gmail.com>
2019-09-18 20:15:38 +0200

    Created product entity

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+use App\Entity\Product;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,7 +16,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = ['A','B','C'];
+        $products = $this->getDoctrine()->getRepository(Product::class)->findAll();
 
         return $this->render('products/index.html.twig', array('products' => $products));
     }
